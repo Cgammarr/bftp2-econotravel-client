34425e82851cb7c611cfae6160930e9cddeac997
AnnaGirona89 <annartworks@gmail.com>
2022-02-21 15:43:01 +0100

    subiendo botones y estilo

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,16 +1,57 @@
 import React from "react";
+import {useEffect, useState} from "react";
+import "./Card.css";
 
 function Card({experience}) {
+    const [experiences, setExperiences] = useState([]);
+    const [requiresUpdate, setRequiresUpdate] = useState(true);
+
+
+    useEffect(() => {
+        if (requiresUpdate) {
+            fetch("http://localhost:8080/api/experiences")
+                .then(r => r.json())
+                .then(setExperiences)
+                .then(_ => setRequiresUpdate(false));
+        }
+    }, [requiresUpdate])
+
+    const deleteExperience = (id) => {
+        fetch(`http://localhost:8080/api/experiences/delete/${id}`,
+            {
+                method: 'DELETE'
+            }
+        ).then(_ => setRequiresUpdate(true))
+
+    }
+
+
     return (
 
         <div className="contenido">
+
             <div className="experience-card">
-                <img className="experienceImg" src={experience.image} alt=""/>
+                <div className="contenedor-img"><img className="experienceImg" src={experience.image} alt=""/></div>
                 <p>{experience.name}</p>
                 <p>{experience.price}</p>
                 <p>{experience.duration}</p>
                 <p>{experience.accessibility}</p>
                 <p>{experience.tags}</p>
+                <table>
+                    <tbody className="experience-table">
+                    <tr className="experienceBtn">
+                        <td>
+                            <a href="#">Editar</a>
+                        </td>
+                        <td>
+                            <a onClick={() => deleteExperience(experience.id)}>Borrar</a>
+                        </td>
+
+                    </tr>
+
+                    </tbody>
+
+                </table>
             </div>
         </div>
 
