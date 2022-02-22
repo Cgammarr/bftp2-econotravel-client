8927ed0adaeae4934297c79d19f1cc12d6034d56
cgammarr <carmen.gamarra@icloud.com>
2022-02-22 14:12:34 +0100

    se anido ruta de añadir experiencias dentro de experiencias

--- src/componentes/Experiences.js
+++ src/componentes/Experiences.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Card from "./Card";
+import {Link} from "react-router-dom";
+
+
 
 function Experiences({experiences}) {
     return (
+
         <div className="experiencies">
-            <h3>aqui ira el catalogo de experiencias</h3>
+            <Link to="/newexperiences">Añadir Experiencias </Link>
 
             {experiences.map ( e => <Card experience={e} /> )}
-
+            <Link to="/newexperiences">Añadir Experiencias </Link>
         </div>
+
+
     )
+
 }
 
 
