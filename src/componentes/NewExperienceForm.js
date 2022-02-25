9813431bcbcc07743318b2c4e136bc27898a4e61
AnnaGirona89 <annartworks@gmail.com>
2022-02-25 09:55:13 +0100

    eliminar new experience form por conflicto

--- src/componentes/NewExperienceForm.js
+++ src/componentes/NewExperienceForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useEffect, useState} from "react";
-import Form from 'react-bootstrap/Form';
+
 
 
 
@@ -37,60 +37,12 @@ function NewExperienceForm() {
 
     }
     return (
+        <span>
 
-         <div className="newExperienceForm">
-             <input  type="text"/>
+        </span>
 
-             <button onClick={addExperience} >experience</button>
-
-
-             <Form>
-                 <>
-                     <Form.Select size="sm">
-                         <option>Small select</option>
-                     </Form.Select>
-                 </>
-                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                     <Form.Label>Experiencia</Form.Label>
-                     <Form.Control type="name" placeholder="Introduce el nombre de la experiencia" />
-
-                 </Form.Group>
-
-                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                     <Form.Label>Nombre</Form.Label>
-                     <Form.Control type="nombre" placeholder="Introduce tu nombre" />
-
-                 </Form.Group>
-
-
-                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                     <Form.Label>Email address</Form.Label>
-                     <Form.Control type="email" placeholder="Enter email" />
-                     <Form.Text className="text-muted">
-                         We'll never share your email with anyone else.
-                     </Form.Text>
-                 </Form.Group>
-
-
-                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                     <Form.Label>Password</Form.Label>
-                     <Form.Control type="password" placeholder="Password" />
-                 </Form.Group>
-                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                     <Form.Check type="checkbox" label="Check me out" />
-                 </Form.Group>
-                 <button onClick={addExperience} variant="primary" type="submit" >
-                     Añadir
-                 </button>
-             </Form>
-
-         </div>
-        );
+         );
     }
 }
 
-
 export default NewExperienceForm;
-
-
-
